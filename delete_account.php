c0dba167b71359e689cbf764641451649197dae3
Antonin PONS <antoninpons@protonmail.com>
2023-05-18 16:06:34 +0200

    commit

--- delete_account.php
+++ delete_account.php
@@ -4,10 +4,11 @@ if (isset($_SESSION['username']) AND isset($_POST['password'])) {
     include('db_class.php');
     $db = new MyDB();
     $pass_hash = hash('sha512', $_POST['password']);
-    $pass_req = $db->prepare("SELECT password FROM Utilisateurs WHERE username=:username;");
+    $pass_req = $db->prepare("SELECT * FROM Utilisateurs WHERE username=:username AND password=:pass_hash;");
     $pass_req->bindValue(':username', $_SESSION['username']);
-    $pass = $pass_req->execute()->fetchArray()['password'];
-    if ($pass_hash === $pass) {
+    $pass_req->bindValue(':pass_hash', $pass_hash);
+    $pass = $pass_req->execute()->fetchArray();
+    if (! empty($pass)) {
         $del_req = $db->prepare("DELETE FROM Utilisateurs WHERE username=:username AND password=:hash;");
         $del_req->bindValue(":username", $_SESSION['username']);
         $del_req->bindValue(":hash", $pass_hash);
