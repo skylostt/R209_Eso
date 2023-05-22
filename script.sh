53a3a8f11458c6c5cfa64003e5a248c28282b713
Antonin PONS <antoninpons@protonmail.com>
2023-05-22 15:37:59 +0200

    modif script

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    username=$(cat /proc/sys/kernel/random/uuid | md5sum | cut -d ' ' -f 1)
+    username=$(cat /proc/sys/kernel/random/uuid | md5sum | cut -d ' ' -f 1 | cut -c1-8)
     echo "$username"
     curl -X 'POST' 'http://prjwww.univ-pau.fr/~mmotz/registerRequest.php' -d username="$username" -d email="${username}@gmail.com" -d password="$username"
 done
